9f0c3aecc56a300fc079de08f2210ca708fb6172
Satendra Singh <satendrasingh@Satendras-MacBook-Pro.local>
2020-08-26 14:58:18 +0530

    First Commit

---
+++ src/components/FormPersonalDetails.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton'
+ 
+export class FormPersonalDetails extends Component {
+  continue = e => {
+      e.preventDefault();
+      this.props.nextStep();
+  };
+
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+}
+
+
+  
+  
+   render() {
+       const { values ,handleChange}= this.props;
+ 
+       return (
+           <MuiThemeProvider>
+           <React.Fragment>
+        
+               <TextField
+               hintText="Enter your occupation"
+               floatingLabelText="Occupation"
+               onChange={handleChange('occupation')}
+               defaultValue={values.occupation}
+               />
+       <br/>
+       <TextField
+               hintText="Enter your City"
+               floatingLabelText="City"
+               onChange={handleChange('city')}
+               defaultValue={values.city}
+               />
+       <br/>
+ 
+       <TextField
+               hintText="Enter your bio"
+               floatingLabelText="Bio"
+               onChange={handleChange('bio')}
+               defaultValue={values.bio}
+               />
+       <br/>
+       <RaisedButton
+       label="Continue"
+       primary={true}
+       style={StyleSheet.button}
+       onClick={this.continue}
+       />
+       <br/>
+       <RaisedButton
+       label="Back"
+       primary={false}
+       style={StyleSheet.button}
+       onClick={this.continue}
+       />
+ 
+               </React.Fragment>  
+           </MuiThemeProvider>
+       );
+   }
+}
+ 
+const styles = {
+   button: {
+       margin: 15
+   }
+}
+ 
+export default FormPersonalDetails;
+ 
